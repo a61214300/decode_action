7177894965ecebf39b7b729c69e628b871be5cfd
action <action@github.com>
2024-08-27 14:50:37 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Tue Aug 27 2024 14:49:42 GMT+0000 (Coordinated Universal Time)
+//Tue Aug 27 2024 14:50:37 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("捷达app"),
