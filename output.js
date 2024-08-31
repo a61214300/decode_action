4aa1f6da492131068bde7843c6207d34e5d191cd
action <action@github.com>
2024-08-31 03:55:19 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sat Aug 31 2024 03:53:54 GMT+0000 (Coordinated Universal Time)
+//Sat Aug 31 2024 03:55:19 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("霸王茶姬"),
